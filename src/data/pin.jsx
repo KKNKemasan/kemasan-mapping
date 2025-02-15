77d567e220c0a3dad0c4dce4bb9b3f3a0c4e51ec
sams <oteotegodok1945@gmail.com>
2025-02-15 07:06:55 +0700

    update url

--- src/data/pin.jsx
+++ src/data/pin.jsx
@@ -4,7 +4,7 @@ const pinData = [
         id: 1,
         name: 'Pecut Samandiman',
         description: 'Pecut Samandiman adalah salah satu UMKM yang berada di kelurahan Kemasan Kota Kediri yang menjual berbagai model pecut mulai ukuran 3 meter dan seterusnya. Pecut ini sangat direkomendasikan untuk kesenian tradisional jaranan.',
-        modelPath: '/assets/models/pin-kereta.glb',
+        modelPath: '/kemasan-mapping/assets/models/pin-kereta.glb',
         position: [5, 0, 30],
         youtube: {
           image: "/yt-icon.png",
@@ -27,7 +27,7 @@ const pinData = [
         id: 2,
         name: 'Kelurahan Kemasan',
         description: "Kelurahan Kemasan Baru",
-        modelPath: '/assets/models/pin-kelurahan.glb',
+        modelPath: '/kemasan-mapping/assets/models/pin-kelurahan.glb',
         position: [65, 0, 30],
         youtube: {
           image: "/yt-icon.png",
@@ -50,7 +50,7 @@ const pinData = [
         id: 3,
         name: 'Makam Ki Ageng Gentiri',
         description: "Nama Mbah Boncolono dikenal sebagai Robin Hood dari Kediri. Julukan pencuri budiman diberikan karena aksinya yang kerap melakukan penjarahan harta para kompeni Belanda beserta antek-anteknya untuk dibagikan kepada rakyat miskin Kediri.",
-        modelPath: '/assets/models/pin-punden.glb',
+        modelPath: '/kemasan-mapping/assets/models/pin-punden.glb',
         position: [75, 0, 30],
         youtube: {
           image: "/yt-icon.png",
@@ -73,7 +73,7 @@ const pinData = [
         id: 4,
         name: 'RUMAH SINGGAH GERILYA JENDRAL SUDIRMAN ',
         description: "Bangunan yang berdiri di Jalan MH.Thamrin No.45, Kelurahan Kemasan, Kota Kediri terkesan biasa, layaknya rumah tua pada umumnya. Rumah tersebut telah dibeli oleh seorang dokter di RSUD Pare yang bernama dr. Yoyok Prasetyo, Sp.OT. Rumah tersebut pernah menjadi tempat persinggahan Panglima Besar Jendral Soedirman. Rumah tua yang pernah disinggahi Jendral Soedirman dalam pelariannya ke Kediri untuk menyusun strategi perang gerilya pada tahun 1948 tersebut, dibeli oleh keluarga Yoyok sekitar 2 tahun lalu. bangunan tersebut terdiri dari ruangan induk, serta dilengkapi dengan paviliun di samping dan sisa tanah yang cukup luas di bangain belakang. Rumah ini memiliki 4 kamar tidur dan menyisakan sumur tua yang masih mempertahankan pengunaan katrol untuk menimba air",
-        modelPath: '/assets/models/pin-kecakot.glb',
+        modelPath: '/kemasan-mapping/assets/models/pin-kecakot.glb',
         position: [66, 0, 16],
         youtube: {
           image: "/yt-icon.png",
@@ -96,7 +96,7 @@ const pinData = [
         id: 5,
         name: 'GUDANG UYAH  ',
         description: "Gudang uyah menjadi asal mula logo gudang garam, merk rokok terkenal di indonesia. Merek rokok ini sudah sangat terkenal di kalangan masyarakat umum bahkan, hingga disebut menjadi salah satu sesepuhnya rokok kretek filter di Indonesia. Anggapan itu didukung dengan mulai adanya pabrik rokok ini berdiri sejak 26 Juni 1958, tepat tiga belas tahun setelah Republik Indonesia merdeka. Berawal dari Tjoa Ing Hwie merupakan peranakan orang China sebagai pendiri pabrik rokok klobot Inghwie, awalnya ia menempa nasib dengan bekerja di pabrik rokok Cap 93 hingga pada tahun 1956, selepas resign dari sana, ia akhirnya berinisiatif membangun pabik rokok klobot Inghwie Lama-kelamaan racikan rokok yang ia buat digemari oleh banyak orang dan berkembang pesat, Tjoa Ing Hwie merubah merk dagangnya menjadi nama gudang garam dan bertahan hingga saat ini. Logo menarik yang jika dilihat sekilas tidak ada hubungan sama sekali dengan produk rokok dengan desain bangunan serupa gudang yang berada di dekat rel kereta api. ",
-        modelPath: '/assets/models/pin-gudang.glb',
+        modelPath: '/kemasan-mapping/assets/models/pin-gudang.glb',
         position: [-15, 0, 0],
         youtube: {
           image: "/yt-icon.png",
@@ -119,7 +119,7 @@ const pinData = [
         id: 6,
         name: 'KIOS IKAN',
         description: "Deskripsi Kios Ikan",
-        modelPath: '/assets/models/pin-ikan.glb',
+        modelPath: '/kemasan-mapping/assets/models/pin-ikan.glb',
         position: [63, 0, 20],
         youtube: {
           image: "/yt-icon.png",
